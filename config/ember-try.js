34943411935a09267322e9fadd63ca101fdb59d5
Ben King <ben@designbybenking.co.uk>
2016-12-27 23:37:03 +0000

    Update ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,17 +1,7 @@
 /*jshint node:true*/
 module.exports = {
+  useVersionCompatibility: true,
   scenarios: [
-    {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      }
-    },
     {
       name: 'ember-lts-2.8',
       bower: {
