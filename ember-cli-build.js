f8b01dd4c4c2bc091360ffb82f57b7b2dc713252
Benjamin Jetschin <b.jetschin@clipmyhorse.tv>
2018-02-26 13:43:37 +0100

    Add 'ember-cli-babel': { includePolyfill: true } to prevent ember try from miserably failing due to reference errors in phantomJS...

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
+    'ember-cli-babel': { includePolyfill: true }
   });
 
   /*
